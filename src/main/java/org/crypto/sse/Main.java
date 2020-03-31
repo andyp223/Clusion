a42506b891283e8bb4b2dd31e17e4e220f6a2beb
andyp223 <andrew_park1@brown.edu>
2020-03-31 19:32:52 -0400

    new edits

---
+++ src/main/java/org/crypto/sse/Main.java
@@ -0,0 +1,112 @@
+package org.crypto.sse;
+
+
+import java.io.*;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.crypto.NoSuchPaddingException;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.TreeMultimap;
+
+import java.io.*;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.crypto.NoSuchPaddingException;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.TreeMultimap;
+
+// Multimap setup 
+
+// update 
+
+// query 
+
+public class Main {
+	// updating the searchable encrypted db
+	public static DynRH2Lev update(String filename, byte[] sk, DynRH2Lev twolev) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, IOException, InvalidKeySpecException {
+		System.out.println("Currently in update phase");
+		ArrayList<File> listOfFile = new ArrayList<File>();
+		TextProc.listf(filename, listOfFile);		
+		TextProc.TextProc(false, filename);
+		TreeMultimap<String, byte[]> tokenUp = DynRH2Lev.tokenUpdate(sk, TextExtractPar.lp1);
+		DynRH2Lev.update(twolev.getDictionaryUpdates(), tokenUp);
+		
+		return twolev;	
+	}
+	
+	
+	// querying the searchable encrypted db 
+	public static List<String> query(String keyword, byte[] sk, DynRH2Lev twolev) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, IOException {
+		System.out.println("Currently in query phase");
+		byte[][] token = DynRH2Lev.genToken(sk, keyword);
+		
+		List<String> hello = DynRH2Lev.resolve(CryptoPrimitives.generateCmac(sk, 3 + new String()),
+				twolev.query(token, twolev.getDictionary(), twolev.getArray(), twolev.getDictionaryUpdates()));
+		
+		return hello;
+	}
+	
+	// create a while loop that is constantly listening for requests. 
+	
+	public static void main(String[] args) throws Exception {
+		String pass = "hello world"; // NOTE: Should this be our key ? 
+		byte[] sk = RR2Lev.keyGen(256, pass, "salt/salt", 100000);
+		
+		System.out.println("Generating searchable map"); 
+		System.out.println("Working Directory = " +
+	              System.getProperty("user.dir"));
+		String pathName = "text-files"; // relative path to the files 
+
+		ArrayList<File> listOfFile = new ArrayList<File>();
+		TextProc.listf(pathName, listOfFile);
+
+		TextProc.TextProc(false, pathName);
+		
+		int bigBlock = 1000;
+		int smallBlock = 100;
+		int dataSize = 10000;
+
+		// // Construction of the global multi-map
+		System.out.println("\nBeginning of Encrypted Multi-map creation \n");
+
+		DynRH2Lev twolev = DynRH2Lev.constructEMMParGMM(sk, TextExtractPar.lp1, bigBlock, smallBlock, dataSize);
+
+		// Empty the previous multimap
+
+		TextExtractPar.lp1 = ArrayListMultimap.create();
+		
+		System.out.println("Buffered Reader Begins Here");
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			String input;
+		    while ((input = br.readLine()) != null) {
+		    		String[] command = input.split(" ", 2);
+		        if (command[0].equals("update")) {
+		        		twolev = update(command[1],sk,twolev);
+		        }
+		        else if (command[0].equals("query")) { // NOTE: we want to return the token note the file name
+		        		System.out.println(query(command[1],sk,twolev));
+		        } else {
+		            System.out.println("ERROR: Invalid Input");
+		            return;
+		        }
+		    }
+		    } catch (IOException ioe) {
+		      // Not possible. No error message can make sense of this.
+		      throw new Error("ERROR: Input exception");
+		    }
+		}		
+}
